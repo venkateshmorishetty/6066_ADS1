e16051471c8aa3ab69ff7d73c72f6a6b19371a1b
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-26 20:24:09 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m3/3-sum/Solution.java
+++ m3/3-sum/Solution.java
@@ -1,31 +1,37 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.Arrays;
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int count =0;
 		int[] arr = new int[size];
-		// int k;
+		int j,k,sum;
 		for(int i = 0; i < size; i++) {
 			arr[i] = sc.nextInt();
 		}
+		Arrays.sort(arr);
 		for(int i =0; i < arr.length-1; i++) {
-			for(int j = i+1; j < arr.length; j++) {
-				// k = j+1;
-				// System.out.println(arr[i] +"   " +arr[j]+"    "+arr[k]);
-				// if(arr[i]+arr[j]+arr[k]==0) {
-				// 	count++;
-				// 	k++;
-				// } else {
-				// 	k++;
-				// }
-				for(int k = j+1; k<arr.length; k++){
-					if(arr[i]+arr[j]+arr[k] == 0) {
-						count++;
-					}
+			k = arr.length-1;
+
+			j=i+1;
+			while(j<k){
+				sum = arr[i]+arr[j]+arr[k];
+				if(sum == 0){
+					k--;
+					count++;
+					j++;
 				}
+				else if(sum < 0) {
+					j++;
+				}
+				else {
+					k--;
+				}	
+
 			}
+				
 		}
 		System.out.println(count);
 	}
