f347cfb62c49b7b7072605da6c409c6a3fbf2d06
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-26 23:03:48 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m3/3-sum/Solution.java
+++ m3/3-sum/Solution.java
@@ -4,39 +4,37 @@ import java.util.Arrays;
  * Class for solution.
  */
 class Solution {
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		int count = 0;
-		int[] arr = new int[size];
-		int j, k, sum;
-		for (int i = 0; i < size; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		for (int i = 0; i < arr.length - 1; i++) {
-			k = arr.length - 1;
-			j = i + 1;
-			while (j < k) {
-				sum = arr[i] + arr[j] + arr[k];
-				if (sum == 0) {
-					k--;
-					count++;
-					j++;
-				} else if (sum < 0) {
-					j++;
-				} else {
-					k--;
-				}	
-
-			}
-				
-		}
-		System.out.println(count);
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int count = 0;
+        int[] arr = new int[size];
+        int j, k, sum;
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            k = arr.length - 1;
+            j = i + 1;
+            while (j < k) {
+                sum = arr[i] + arr[j] + arr[k];
+                if (sum == 0) {
+                    k--;
+                    count++;
+                    j++;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
