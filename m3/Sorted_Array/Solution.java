e0316e3e78ab026c682d50e5f52b51cfd269f1e7
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-26 22:59:30 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m3/Sorted_Array/Solution.java
+++ m3/Sorted_Array/Solution.java
@@ -4,62 +4,68 @@ import java.util.Arrays;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		int size1, size2;
-		Scanner sc = new Scanner(System.in);
-		size1 = sc.nextInt();
-		size2 = sc.nextInt();
-		String[] arraystring1 = new String[size1];
-		if (size1 > 0) {
-			arraystring1 = sc.next().split(",");
-		} else {
-			sc.nextLine();
-		}
-		String[] arraystring2 = sc.next().split(",");
-		int[] arr1 = new int[size1];
-		int[] arr2 = new int[size2];
-		int newsize = size1 + size2;
-		int[] res = new int[newsize];
-		for (int j = 0; j < size1; j++) {
-			arr1[j] = Integer.parseInt(arraystring1[j]);
-		}
-		for (int j = 0; j < size2; j++) {
-			arr2[j] = Integer.parseInt(arraystring2[j]);
-		}
-		int first = 0, second = 0;
-		for (int i = 0; i < newsize; i++) {
-			if (size1 > 0 && size2 > 0 && first<arr1.length && second < arr2.length) {
-				if (arr1[first] > arr2[second]) {
-					res[i] = arr2[second];
-					second++;
-				}
-				else if (arr1[first] < arr2[second]) {
-					res[i] = arr1[first];
-					first++;
-				} else {
-					res[i] = arr1[first];
-					first++;
-					i++;
-					res[i] = arr2[second];
-					second++;
-				}
-			} else {
-				if(arr1.length > arr2.length) {
-					res[i] = arr1[first];
-					first++;
-				} else {
-					res[i] = arr2[second];
-					second++;
-				}
-			}
-		}
-		for (int i = 0; i < newsize - 1; i++) {
-			System.out.print(res[i] + ",");
-		} System.out.print(res[newsize - 1]);
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //pass
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        int size1, size2;
+        Scanner sc = new Scanner(System.in);
+        size1 = sc.nextInt();
+        size2 = sc.nextInt();
+        String[] arraystring1 = new String[size1];
+        if (size1 > 0) {
+            arraystring1 = sc.next().split(",");
+        } else {
+            sc.nextLine();
+        }
+        String[] arraystring2 = sc.next().split(",");
+        int[] arr1 = new int[size1];
+        int[] arr2 = new int[size2];
+        int newsize = size1 + size2;
+        int[] res = new int[newsize];
+        for (int j = 0; j < size1; j++) {
+            arr1[j] = Integer.parseInt(arraystring1[j]);
+        }
+        for (int j = 0; j < size2; j++) {
+            arr2[j] = Integer.parseInt(arraystring2[j]);
+        }
+        int first = 0, second = 0;
+        for (int i = 0; i < newsize; i++) {
+            if (size1 > 0 && size2 > 0 && first < arr1.length
+                && second < arr2.length) {
+                if (arr1[first] > arr2[second]) {
+                    res[i] = arr2[second];
+                    second++;
+                } else if (arr1[first] < arr2[second]) {
+                    res[i] = arr1[first];
+                    first++;
+                } else {
+                    res[i] = arr1[first];
+                    first++;
+                    i++;
+                    res[i] = arr2[second];
+                    second++;
+                }
+            } else {
+                if (arr1.length > arr2.length) {
+                    res[i] = arr1[first];
+                    first++;
+                } else {
+                    res[i] = arr2[second];
+                    second++;
+                }
+            }
+        }
+        for (int i = 0; i < newsize - 1; i++) {
+            System.out.print(res[i] + ",");
+        } System.out.print(res[newsize - 1]);
+    }
 }
