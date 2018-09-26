62205f8200ad22e955e080f27a67a7a68eb6dc71
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-26 23:03:14 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m3/3-sum/Solution.java
+++ m3/3-sum/Solution.java
@@ -1,32 +1,36 @@
-import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
-		int count =0;
+		int count = 0;
 		int[] arr = new int[size];
-		int j,k,sum;
-		for(int i = 0; i < size; i++) {
+		int j, k, sum;
+		for (int i = 0; i < size; i++) {
 			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
-		for(int i =0; i < arr.length-1; i++) {
-			k = arr.length-1;
-
-			j=i+1;
-			while(j<k){
-				sum = arr[i]+arr[j]+arr[k];
-				if(sum == 0){
+		for (int i = 0; i < arr.length - 1; i++) {
+			k = arr.length - 1;
+			j = i + 1;
+			while (j < k) {
+				sum = arr[i] + arr[j] + arr[k];
+				if (sum == 0) {
 					k--;
 					count++;
 					j++;
-				}
-				else if(sum < 0) {
+				} else if (sum < 0) {
 					j++;
-				}
-				else {
+				} else {
 					k--;
 				}	
 
