a5adcd2220cb726b0b16fcc5937b73fb6b897ec1
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-26 22:40:10 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

---
+++ m3/Sorted_Array/Solution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+class Solution {
+	public static void main(String[] args) {
+		int size1, size2;
+		Scanner sc = new Scanner(System.in);
+		size1 = sc.nextInt();
+		size2 = sc.nextInt();
+		sc.nextLine();
+		String[] arraystring1 = sc.next().split(",");
+		String[] arraystring2 = sc.next().split(",");
+		int[] arr1 = new int[size1];
+		int[] arr2 = new int[size2];
+		int newsize =size1+size2;
+		int[] res = new int[newsize];
+		for(int j = 0; j < size1; j++) {
+			arr1[j] = Integer.parseInt(arraystring1[j]);
+		}
+		for(int j = 0; j < size2; j++) {
+			arr2[j] = Integer.parseInt(arraystring2[j]);
+		}
+		int first=0,second=0;
+		for(int i = 0; i < newsize; i++) {
+			if(first<arr1.length && second < arr2.length) {
+				if(arr1[first]>arr2[second]) {
+					res[i] = arr2[second];
+					second++;
+				}
+				else if(arr1[first] < arr2[second]) {
+					res[i] = arr1[first];
+					first++;
+				} else {
+					res[i] = arr1[first];
+					first++;
+					i++;
+					res[i] = arr2[second];
+					second++;
+				}
+			} else {
+				if(arr1.length > arr2.length) {
+					res[i] = arr1[first];
+					first++;
+				} else {
+					res[i] = arr2[second];
+					second++;
+				}
+			}
+		}
+		for(int i = 0; i < newsize - 1; i++) {
+			System.out.print(res[i]+",");
+		}System.out.print(res[newsize - 1]);
+	}
+}
