79c63e417672c36a1ff7f1075799258333dc71da
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-27 21:42:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/4.1 Balanced Paranthesis/Linkedlist.java
+++ m4/4.1 Balanced Paranthesis/Linkedlist.java
@@ -1,32 +1,75 @@
-import java.uil.Scanner;
+import java.util.Scanner;
 class Node {
-	String item;
-	Node address;
-	public Node(String item) {
+	Node() {
+
+	}
+	int item;
+	Node address = null;
+	public Node(int item) {
 		this.item = item;
-		adress = null;
+	}
+	public void setAddress(Node t) {
+		address = t;
+	}
+}
+class list{
+	Node head;
+	Node tail;
+	public void insert(int ele) {
+		Node n = new Node(ele);
+		if(head == null) {
+			head = n;
+			tail = n;
+		} else {
+			tail.address = n;
+			tail = n;
+		}
+	}
+	public void delend() {
+		Node temp = head;
+		while(temp.address!= tail) {
+			temp = temp.address;
+		}
+		temp.address = null;
+		tail = temp;
+	}
+	public void delbeg() {
+		head = head.address;
+	}
+	public void delloc(int data) {
+		Node temp = head;
+		while(temp.address.item != data) {
+			temp = temp.address;
+		}
+		temp.address = temp.address.address;
+
+	}
+	public void print() {
+		Node temp = head;
+		while(temp != null) {
+			System.out.println(temp.item);
+			temp = temp.address;
+		}
 	}
 }
 class Linkedlist {
 	public static void main(String[] args) {
-		Node n;
-		Node head,tail;
-		Scanner sc = new Scanner(System.in);
-		int lines = sc.nextInt();
-		for(int i = 0; i < lines; i++) {
-			String  line = sc.next();
-			String[] arr = line.split("", line.length());
-			for(int j = 0; j < arr.length; j++) {
-				if(arr[j].equals("{") || arr[j].equals("[") || arr[j].equals("(")) {
-					n = new Node(arr[j]);
-					if(head.getAddress() == null) {
-						head = n;
-						tail = head;
-					} else {
-						
-					}
-				}
-			}
-		}
+		list l = new list();
+		l.insert(2);
+		l.insert(3);
+		l.insert(4);
+		l.print();
+		System.out.println("++++++++++++++====");
+		l.delend();
+		l.print();
+		l.delbeg();
+		System.out.println("+++++++++++++++++++======");
+		l.print();
+		l.insert(5);
+		l.insert(6);
+		l.insert(9);
+		l.delloc(6);
+		System.out.println("@@@@@@@@@@@");
+		l.print();
 	}
 }
