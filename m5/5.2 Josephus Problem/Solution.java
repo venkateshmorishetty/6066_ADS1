4da9d72ab5c1bbfa946e50d141cf10c83df7335c
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-28 22:15:20 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+class node {
+	int data;
+	node next;
+	node(int d) {
+		data = d;
+	}
+}
+class linkedlist {
+	int size;
+	int count;
+	node head,tail;
+	String result ="";
+	linkedlist(int s, int c) {
+		size = s;
+		count = c;
+	}
+
+	public void josephproblem() {
+		node newnode = new node(0);
+		head = newnode;
+		tail = newnode;
+		for(int i = 1; i < size; i++) {
+			node n = new node(i);
+			tail.next = n;
+			tail = n;
+		}
+		int c;
+		node temp;
+		// System.out.println(tail.data);
+		while(size != 1) {
+			for(c = 1; c < count; c++) {
+				temp = head;
+				head = head.next;
+				tail.next = temp;
+				tail = temp;
+			}
+			result += head.data+" ";
+			head = head.next;
+			c = 1;
+			size--;
+		}
+		result += " "+head.data;
+		System.out.println(result);
+	}
+}
+class Solution{
+	public static void main(String[] args) {
+		linkedlist l;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.nextLine();
+		for(int i = 0; i < n; i++) {
+			String[] tokens = sc.nextLine().split(" ");
+			l = new linkedlist(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+			l.josephproblem();
+		}
+	}
+	
+}
