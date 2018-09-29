11a1d2407f6a70ac874e1ab8597145b5cec0d3ed
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-29 16:55:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m5/5.2 Josephus Problem/Solution.java
+++ m5/5.2 Josephus Problem/Solution.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
  * { solution class }.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//pass.
-	}
-	/**
-	 * { main }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		linkedlist l;
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String[] tokens = sc.nextLine().split(" ");
-			l = new linkedlist(Integer.parseInt(tokens[0]),
-				Integer.parseInt(tokens[1]));
-			l.josephproblem();
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //pass.
+    }
+    /**
+     * { main }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        linkedlist l;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String[] tokens = sc.nextLine().split(" ");
+            l = new linkedlist(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]));
+            l.josephproblem();
+        }
+    }
 }
