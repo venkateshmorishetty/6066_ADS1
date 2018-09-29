9a2f863d5a33580b883235236a66ccb9c04415d9
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-09-29 16:30:54 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m5/5.1 Deque/Solution.java
+++ m5/5.1 Deque/Solution.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 class Solution {
-	public static void main(String[] args) {
-		linkedlist l = new linkedlist();
-		Scanner sc = new Scanner(System.in);
-		int lines = sc.nextInt();
-		for(int t = 0; t <= lines; t++) {
-			String s = sc.nextLine();
-			String[] tokens = s.split(" ");
-			switch(tokens[0]) {
-				case "isEmpty":
-				System.out.println(l.isEmpty());
-					break;
-				case "size":
-				System.out.println(l.size());
-					
-					break;
-				case "pushLeft":
-					l.pushLeft(Integer.parseInt(tokens[1]));
-					break;
-				case "pushRight":
-					l.pushRight(Integer.parseInt(tokens[1]));
-					break;
-				case "popLeft":
-					l.popLeft();
-					break;
-				case "popRight":
-					l.popRight();
-					break;
-				default:
-					break;			
-			}
-		}
-	}
+    public static void main(String[] args) {
+        linkedlist l = new linkedlist();
+        Scanner sc = new Scanner(System.in);
+        int lines = sc.nextInt();
+        for(int t = 0; t <= lines; t++) {
+            String s = sc.nextLine();
+            String[] tokens = s.split(" ");
+            switch(tokens[0]) {
+                case "isEmpty":
+                System.out.println(l.isEmpty());
+                    break;
+                case "size":
+                System.out.println(l.size());
+                    break;
+                case "pushLeft":
+                    l.pushLeft(Integer.parseInt(tokens[1]));
+                    break;
+                case "pushRight":
+                    l.pushRight(Integer.parseInt(tokens[1]));
+                    break;
+                case "popLeft":
+                    l.popLeft();
+                    break;
+                case "popRight":
+                    l.popRight();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
