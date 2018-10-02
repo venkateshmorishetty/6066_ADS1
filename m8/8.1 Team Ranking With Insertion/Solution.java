0409e39e344c6483b88117ac68f569d0352ed4fa
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-02 18:28:20 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m8/8.1 Team Ranking With Insertion/Solution.java
+++ m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Team{
+class Team {
 	String name;
 	int wins;
 	int loss;
@@ -11,13 +11,13 @@ class Team{
 		draws = d;
 	}
 	public int compareTo(Team that) {
-		if(this.wins < that.wins) {
+		if (this.wins < that.wins) {
 			return 1; 
-		} else if(this.wins == that.wins) {
-			if(this.loss > that.loss) {
+		} else if (this.wins == that.wins) {
+			if (this.loss > that.loss) {
 				return 1;
-			} else if(this.loss == that.loss) {
-				if(this.draws < that.draws) {
+			} else if (this.loss == that.loss) {
+				if (this.draws < that.draws) {
 					return 1;
 				} else {
 					return 0;
@@ -34,35 +34,28 @@ class Teams {
 	Team[] t = new Team[10];
 	int size = 0;
 	public void makingTeamobj(String[] l) {
-		
 		Team newteam = new Team(l[0], Integer.parseInt(l[1]), Integer.parseInt(l[2]), Integer.parseInt(l[3]));
 		t[size++] = newteam;
 
 	}
 	public void sorting() {
 		int min;
-		for(int i = 0; i < size - 1; i++) {
+		for (int i = 0; i < size - 1; i++) {
 			min = i;
 			int j = i + 1;
-			while(j<=size - 1) {
+			while (j <= size - 1) {
 				int check = t[min].compareTo(t[j]);
-				if(check == 1) {
+				if (check == 1) {
 					min = j;
 				}
 				j++; 
 			}
-
 			Team temp = t[i];
 			t[i] = t[min];
 			t[min] = temp;
 		}
 	}
 }
-
-
-
-
-
 class Solution {
 	public static void main(String[] args) {
 		Teams teams = new Teams();
@@ -72,8 +65,9 @@ class Solution {
 			teams.makingTeamobj(line);
 		}
 		teams.sorting();
-		for(int i = 0; i < teams.size-1; i++) {
-			System.out.print(teams.t[i].name+",");
-		}System.out.print(teams.t[teams.size-1].name);
+		for (int i = 0; i < teams.size - 1; i++) {
+			System.out.print(teams.t[i].name + ",");
+		}
+		System.out.print(teams.t[teams.size - 1].name);
 	}
 }
