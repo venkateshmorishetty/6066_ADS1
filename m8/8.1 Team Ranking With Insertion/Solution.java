4a8e30f7a7bc4da72d4a827dddd5f2adf4f49e1a
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-02 18:57:58 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m8/8.1 Team Ranking With Insertion/Solution.java
+++ m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,140 +1,155 @@
 import java.util.Scanner;
+/**
+ * Class for team.
+ */
 class Team {
-	/**
-	 * { name of the team }.
-	 */
-	private String name;
-	/**
-	 * { number of wins }.
-	 */
-	private int wins;
-	/**
-	 * { number of losses }.
-	 */
-	private int loss;
-	/**
-	 * { number of draws }.
-	 */
-	private int draws;
-	Team(final String n, final int w, final int l, final int d) {
-		name = n;
-		wins = w;
-		loss = l;
-		draws = d;
-	}
-	/**
-	 * { returns the name }.
-	 *
-	 * @return     { name of the team }.
-	 */
-	public String getname() {
-		return name;
-	}
-	/**
-	 * { compare current obj with that obj }.
-	 *
-	 * @param      that  The that
-	 *
-	 * @return     { -1 if less 0 equal +1 grater }.
-	 */
-	public int compareTo(final Team that) {
-		if (this.wins < that.wins) {
-			return 1; 
-		} else if (this.wins == that.wins) {
-			if (this.loss > that.loss) {
-				return 1;
-			} else if (this.loss == that.loss) {
-				if (this.draws < that.draws) {
-					return 1;
-				} else {
-					return 0;
-				}
-			} else {
-				return 0;
-			}
-		} else {
-			return 0;
-		}
-	}
+    /**
+     * { name of the team }.
+     */
+    private String name;
+    /**
+     * { number of wins }.
+     */
+    private int wins;
+    /**
+     * { number of losses }.
+     */
+    private int loss;
+    /**
+     * { number of draws }.
+     */
+    private int draws;
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { name of the team }.
+     * @param      w     { wins of the team }
+     * @param      l     { losses of the team }
+     * @param      d     { draws of the team }
+     */
+    Team(final String n, final int w, final int l, final int d) {
+        name = n;
+        wins = w;
+        loss = l;
+        draws = d;
+    }
+    /**
+     * { returns the name }.
+     *
+     * @return     { name of the team }.
+     */
+    public String getname() {
+        return name;
+    }
+    /**
+     * { compare current obj with that obj }.
+     *
+     * @param      that  The that
+     *
+     * @return     { -1 if less 0 equal +1 grater }.
+     */
+    public int compareTo(final Team that) {
+        if (this.wins < that.wins) {
+            return 1; 
+        } else if (this.wins == that.wins) {
+            if (this.loss > that.loss) {
+                return 1;
+            } else if (this.loss == that.loss) {
+                if (this.draws < that.draws) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
 /**
  * Class for teams.
  */
 class Teams {
-	/**
-	 * { teams object array }.
-	 */
-	Team[] t = new Team[10];
-	/**
-	 * { size of the array }.
-	 */
-	private int size = 0;
-	/**
-	 * { making team object }.
-	 *
-	 * @param      l     { parameter_description }
-	 */
-	public void makingTeamobj(final String[] l) {
-		Team newteam = new Team(l[0], Integer.parseInt(l[1]),
-			Integer.parseInt(l[2]), Integer.parseInt(l[2 + 1]));
-		t[size++] = newteam;
+    /**
+     * { initial size }.
+     */
+    private int initialsize  = 10;
+    /**
+     * { teams object array }.
+     */
+    Team[] t = new Team[initialsize];
+    /**
+     * { size of the array }.
+     */
+    private int size = 0;
+    /**
+     * { making team object }.
+     *
+     * @param      l     { parameter_description }
+     */
+    public void makingTeamobj(final String[] l) {
+        Team newteam = new Team(l[0], Integer.parseInt(l[1]),
+            Integer.parseInt(l[2]), Integer.parseInt(l[2 + 1]));
+        t[size++] = newteam;
 
-	}
-	/**
-	 * { sorts the array of objects }.
-	 */
-	public void sorting() {
-		int min;
-		for (int i = 0; i < size - 1; i++) {
-			min = i;
-			int j = i + 1;
-			while (j <= size - 1) {
-				int check = t[min].compareTo(t[j]);
-				if (check == 1) {
-					min = j;
-				}
-				j++;
-			}
-			Team temp = t[i];
-			t[i] = t[min];
-			t[min] = temp;
-		}
-	}
-	/**
-	 * Gets the size.
-	 *
-	 * @return     The size.
-	 */
-	public int getSize() {
-		return size;
-	}
+    }
+    /**
+     * { sorts the array of objects }.
+     */
+    public void sorting() {
+        int min;
+        for (int i = 0; i < size - 1; i++) {
+            min = i;
+            int j = i + 1;
+            while (j <= size - 1) {
+                int check = t[min].compareTo(t[j]);
+                if (check == 1) {
+                    min = j;
+                }
+                j++;
+            }
+            Team temp = t[i];
+            t[i] = t[min];
+            t[min] = temp;
+        }
+    }
+    /**
+     * Gets the size.
+     *
+     * @return     The size.
+     */
+    public int getSize() {
+        return size;
+    }
 }
 /**
  * { solution class }.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//constructor.
-	}
-	/**
-	 * { main function }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Teams teams = new Teams();
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			String[] line = sc.nextLine().split(",");
-			teams.makingTeamobj(line);
-		}
-		teams.sorting();
-		for (int i = 0; i < teams.getSize() - 1; i++) {
-			System.out.print(teams.t[i].getname() + ",");
-		}
-		System.out.print(teams.t[teams.getSize() - 1].getname());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor.
+    }
+    /**
+     * { main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Teams teams = new Teams();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String[] line = sc.nextLine().split(",");
+            teams.makingTeamobj(line);
+        }
+        teams.sorting();
+        for (int i = 0; i < teams.getSize() - 1; i++) {
+            System.out.print(teams.t[i].getname() + ",");
+        }
+        System.out.print(teams.t[teams.getSize() - 1].getname());
+    }
 }
