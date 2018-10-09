d08b617440f5349b5fe7dbd2921c48e65a3c9e11
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-09 20:55:10 +0530

    "ads1_m14_a2: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m14/14.2 student reservation system/Solution.java
+++ m14/14.2 student reservation system/Solution.java
@@ -23,7 +23,7 @@ final class Solution {
 			String[] date = arr[1].split("-");
 			int[] dat = new int[date.length];
 			for (int j = 0; i < date.length; j++) {
-				dat[j] = Integer.parseInt(date[j]);
+				dat[j] = Integer.parseInt(date[i]);
 			}
 			// Date d = new Date(Integer.parseInt(date[0]), Integer.parseInt(date[1]), Integer.parseInt(date[2]));
 			student st = new student(arr[0], dat, Integer.parseInt(arr[2]),
