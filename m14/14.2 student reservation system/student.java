f8a341c9080137776425dd38d704cd9b165f449a
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-09 18:05:51 +0530

    "ads1_m14_a2: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m14/14.2 student reservation system/student.java
+++ m14/14.2 student reservation system/student.java
@@ -28,7 +28,7 @@ class student {
 			} else if(s1.marks2 - s2.marks2 != 0) {
 				return s1.marks2 - s2.marks2;
 			} else {
-				return s2.birth.compareTo(s1.birth);
+				return s1.birth.compareTo(s2.birth);
 			}
 		}
 	};
