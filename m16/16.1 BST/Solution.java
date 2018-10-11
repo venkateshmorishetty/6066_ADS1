780f1b5661bf346d6311610e7fd193c656aa8fb3
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-11 19:19:54 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m16/16.1 BST/Solution.java
+++ m16/16.1 BST/Solution.java
@@ -1,59 +1,151 @@
 import java.util.Scanner;
+/**
+ * Class for book.
+ */
 class Book {
+	/**
+	 * { name of the book }.
+	 */
 	private String name;
+	/**
+	 * { author name }.
+	 */
 	private String author;
+	/**
+	 * { price of the book }.
+	 */
 	private Double price;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     { name }
+	 * @param      a     { author }
+	 * @param      p     { price }
+	 */
 	Book(String n, String a, double p) {
 		name = n;
 		author = a;
 		price = p;
 	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * Gets the author.
+	 *
+	 * @return     The author.
+	 */
 	public String getAuthor() {
 		return author;
 	}
+	/**
+	 * Gets the price.
+	 *
+	 * @return     The price.
+	 */
 	public Double getPrice() {
 		return price;
 	}
 }
-
-
+/**
+ * Class for node.
+ *
+ * @param      <Value>  The value
+ */
 class Node<Value> {
+	/**
+	 * { left node address }.
+	 */
 	private Node left = null;
+	/**
+	 * { book key }.
+	 */
 	private Book book;
+	/**
+	 * { qty value }.
+	 */
 	private Value qty;
+	/**
+	 * { right node address }.
+	 */
 	private Node right = null;
-	Node(Book b, Value q) {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      b     { key }
+	 * @param      q     { value }
+	 */
+	Node(final Book b, final Value q) {
 		qty = q;
 		book = b;
 	}
+	/**
+	 * Gets the left.
+	 *
+	 * @return     The left.
+	 */
 	public Node getLeft() {
 		return left;
 	}
+	/**
+	 * Gets the right.
+	 *
+	 * @return     The right.
+	 */
 	public Node getRight() {
 		return right;
 	}
+	/**
+	 * Gets the qty.
+	 *
+	 * @return     The qty.
+	 */
 	public Value getQty() {
 		return qty;
 	}
+	/**
+	 * Gets the book.
+	 *
+	 * @return     The book.
+	 */
 	public Book getBook() {
 		return book;
 	}
-
+	/**
+	 * Sets the right.
+	 *
+	 * @param      n     { set the right node address }
+	 */
 	public void setRight(Node n) {
 		right = n;
 	}
-
+	/**
+	 * Sets the left.
+	 *
+	 * @param      n     { set left node address }
+	 */
 	public void setLeft(Node n) {
 		left = n;
 	}
+	/**
+	 * Sets the qty.
+	 *
+	 * @param      q     The quarter
+	 */
 	public void setQty(Value q) {
 		qty = q;
 	}
 }
-
+/**
+ * Class for binary search tree.
+ *
+ * @param      <Value>  The value
+ */
 class BinarySearchTree<Value> {
 	Node head = null;
 	public void put(Book key, int value) {
@@ -99,14 +191,24 @@ class BinarySearchTree<Value> {
 		return null;
 	}
 }
-
-class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+final class Solution {
+	private Solution() {
+		//constructor.
+	}
+	/**
+	 * { main method }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		BinarySearchTree<Integer> btree = new BinarySearchTree<Integer>();
-		while(sc.hasNext()) {
+		while (sc.hasNext()) {
 			String[] line = sc.nextLine().split(",");
-			switch(line[0]){
+			switch (line[0]) {
 				case "put":
 				Book b = new Book(line[1], line[2], Double.parseDouble(line[3]));
 				btree.put(b, Integer.parseInt(line[4]));
