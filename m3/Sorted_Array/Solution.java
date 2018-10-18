26043e1a8383cc0bfc16525f545af878129b5f37
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-18 18:12:24 +0530

    "ads1_sorted_array: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m3/Sorted_Array/Solution.java
+++ m3/Sorted_Array/Solution.java
@@ -1,40 +1,17 @@
 import java.util.Scanner;
-/**
- * Class for solution.
- */
-final class Solution {
-    /**
-     * Constructs the object.
-     */
-    private Solution() {
-        //pass
-    }
-    /**
-     * { solution class }.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        int size1, size2;
-        Scanner sc = new Scanner(System.in);
-        size1 = sc.nextInt();
-        size2 = sc.nextInt();
-        String[] arraystring1 = new String[size1];
-        if (size1 > 0) {
-            arraystring1 = sc.next().split(",");
-        } else {
-            sc.nextLine();
-        }
-        String[] arraystring2 = sc.next().split(",");
+class SortedArray {
+    public void sortedArray(String[] array1, String[] array2) {
+        int size1 = array1.length;
+        int size2 = array2.length;
         int[] arr1 = new int[size1];
         int[] arr2 = new int[size2];
         int newsize = size1 + size2;
         int[] res = new int[newsize];
         for (int j = 0; j < size1; j++) {
-            arr1[j] = Integer.parseInt(arraystring1[j]);
+            arr1[j] = Integer.parseInt(array1[j]);
         }
         for (int j = 0; j < size2; j++) {
-            arr2[j] = Integer.parseInt(arraystring2[j]);
+            arr2[j] = Integer.parseInt(array2[j]);
         }
         int first = 0, second = 0;
         for (int i = 0; i < newsize; i++) {
@@ -66,5 +43,37 @@ final class Solution {
         for (int i = 0; i < newsize - 1; i++) {
             System.out.print(res[i] + ",");
         } System.out.print(res[newsize - 1]);
+
+    }
+}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //pass
+    }
+    /**
+     * { solution class }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        int size1, size2;
+        Scanner sc = new Scanner(System.in);
+        size1 = sc.nextInt();
+        size2 = sc.nextInt();
+        String[] arraystring1 = new String[size1];
+        if (size1 > 0) {
+            arraystring1 = sc.next().split(",");
+        } else {
+            sc.nextLine();
+        }
+        String[] arraystring2 = sc.next().split(",");
+        SortedArray s = new SortedArray();
+        s.sortedArray(arraystring1, arraystring2);
     }
 }
