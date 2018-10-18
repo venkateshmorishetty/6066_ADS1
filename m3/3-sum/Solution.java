da87732d07c934400217db78aaeb5983f044e5d7
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-18 17:51:39 +0530

    "ads1_3sum: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m3/3-sum/Solution.java
+++ m3/3-sum/Solution.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
+class ThreeSum {
+    public int ThreeSumCount(int[] arr) {
+        int count = 0;
+        int j, k, sum;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            k = arr.length - 1;
+            j = i + 1;
+            while (j < k) {
+                sum = arr[i] + arr[j] + arr[k];
+                if (sum == 0) {
+                    k--;
+                    count++;
+                    j++;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+}
 /**
  * Class for solution.
  */
@@ -18,29 +42,11 @@ final class Solution {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int count = 0;
         int[] arr = new int[size];
-        int j, k, sum;
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length - 1; i++) {
-            k = arr.length - 1;
-            j = i + 1;
-            while (j < k) {
-                sum = arr[i] + arr[j] + arr[k];
-                if (sum == 0) {
-                    k--;
-                    count++;
-                    j++;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
-                }
-            }
-        }
-        System.out.println(count);
+        ThreeSum ts = new ThreeSum();       
+        System.out.println(ts.ThreeSumCount(arr));
     }
 }
