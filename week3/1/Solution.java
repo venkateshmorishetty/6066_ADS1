6100131a4e76f66e505d1bedc707b340bb9d985a
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-10-27 11:14:33 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- week3/1/Solution.java
+++ week3/1/Solution.java
@@ -1,21 +1,62 @@
 import java.util.Scanner;
+/**
+ * Class for student.
+ */
 class Student {
-	String name;
-	double score;
-	public Student(String n, double s) {
+	/**
+	 * { name of the student }.
+	 */
+	private String name;
+	/**
+	 * score.
+	 */
+	private double score;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     { name  }
+	 * @param      s     { score }
+	 */
+	public Student(final String n, final double s) {
 		name = n;
 		score = s;
 	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * Gets the score.
+	 *
+	 * @return     The score.
+	 */
+	public double getScore() {
+		return score;
+	}
 }
-class Solution {
-	public static void main(String[] args) {
+/**
+ * { solution class }.
+ */
+final class Solution {
+	/**
+	 * { main function }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
-		LinearProbingHashST<Integer, Student> linear = new LinearProbingHashST<Integer, Student>();
+		LinearProbingHashST<Integer, Student> linear = new LinearProbingHashST
+		<Integer, Student>();
 		int total = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < total; i++) {
 			String[] studentinfo = sc.nextLine().split(",");
-			linear.put(Integer.parseInt(studentinfo[0]), new Student(studentinfo[1],
-			           Double.parseDouble(studentinfo[2])));
+			linear.put(Integer.parseInt(studentinfo[0]),
+				new Student(studentinfo[1],
+					Double.parseDouble(studentinfo[2])));
 		}
 		int output = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < output; i++) {
@@ -25,9 +66,9 @@ class Solution {
 				System.out.println("Student doesn't exists...");
 			} else {
 				if (Integer.parseInt(temp[2]) == 1) {
-					System.out.println(t.name);
+					System.out.println(t.getName());
 				} else if (Integer.parseInt(temp[2]) == 2) {
-					System.out.println(t.score);
+					System.out.println(t.getScore());
 				}
 			}
 
